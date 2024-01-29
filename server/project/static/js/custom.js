ec2cdecceec5b81e26e909e74b54f27ae5296c57
lavrovartem <munfc@mail.ru>
2024-01-29 15:21:32 +0500

    Реализация проекта на Django "POTOLOK.PRO" Рефакторинг кода

--- server/project/static/js/custom.js
+++ server/project/static/js/custom.js
@@ -1,26 +1,31 @@
-$(document).ready(function() {
-  $('#phone').inputmask("+7(999)-999-99-99");
+document.addEventListener('DOMContentLoaded', function () {
+  var phoneInput = document.getElementById('id_phone_number');
+  var maskOptions = {
+    mask: '+{7}(000)-000-00-00'
+  };
+  var mask = IMask(phoneInput, maskOptions);
 });
 
-$(document).ready(function() {
-  $('#submit-btn').click(function(e) {
-    e.preventDefault(); // prevent the default action
-    $.ajax({
-      url: '/',
-      type: 'POST',
-      headers: {
-        'X-CSRFToken': $('input[name=csrfmiddlewaretoken]').val()
-      },
-      data: $('#form').serialize(),
-      success: function(response) {
-        if (response.success) {
-          $('#exampleModal').modal('show');
-          $('#form')[0].reset();
-        } else {
-          // Handle errors here
-        }
-      }
-    });
-  });
-});
+
+// $(document).ready(function() {
+//   $('#submit-btn').click(function(e) {
+//     e.preventDefault(); // prevent the default action
+//     $.ajax({
+//       url: '/',
+//       type: 'POST',
+//       headers: {
+//         'X-CSRFToken': $('input[name=csrfmiddlewaretoken]').val()
+//       },
+//       data: $('#form').serialize(),
+//       success: function(response) {
+//         if (response.success) {
+//           $('#exampleModal').modal('show');
+//           $('#form')[0].reset();
+//         } else {
+//           // Handle errors here
+//         }
+//       }
+//     });
+//   });
+// });
 
